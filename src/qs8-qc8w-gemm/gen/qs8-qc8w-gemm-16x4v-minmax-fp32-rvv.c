decfdadbf732cf9320895442609f8e3f208aca70
Miles Rusch <miles.rusch@berkeley.edu>
2025-08-11 13:44:47 -0700

    square opu gemm inner loop

--- src/qs8-qc8w-gemm/gen/qs8-qc8w-gemm-16x4v-minmax-fp32-rvv.c
+++ src/qs8-qc8w-gemm/gen/qs8-qc8w-gemm-16x4v-minmax-fp32-rvv.c
@@ -55,19 +55,15 @@ void xnn_qs8_qc8w_gemm_minmax_fp32_ukernel_16x4v__rvv(
     w = (const int32_t*) w + nr;
 
     size_t k = kc;
+    __asm__ volatile("vsetvli zero, %0, e8, m1, ta, ma" : : "r"(vl));
     do {
-      // printf("    k=%zu\n", k);
-      
-      __asm__ volatile("vsetvli zero, %0, e8, m1, ta, ma" : : "r"(mr));
       __asm__ volatile("vlse8.v v8, (%0), %1" : : "r"(a0), "r"(a_stride));
       a0++;
-      __asm__ volatile("vsetvli zero, %0, e8, m1, ta, ma" : : "r"(vl));
       __asm__ volatile("vle8.v v9, (%0)" : : "r"(w));
       
       w = (const int8_t*) w + nr;
       
       VOPACC(m1, v8, v9);
-      // __asm__ volatile("vmacc.vv	v0,v8,v9");
 
       k -= sizeof(int8_t);
     } while (k != 0);

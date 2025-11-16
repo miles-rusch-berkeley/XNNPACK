2d23968835c27979c2814eeedd70a80cbf81e93c
Miles Rusch <miles.rusch@berkeley.edu>
2025-11-16 14:41:04 -0800

    passing fc_test

--- src/qs8-qc8w-gemm/gen/qs8-qc8w-gemm-16x4v-minmax-fp32-rvv.c
+++ src/qs8-qc8w-gemm/gen/qs8-qc8w-gemm-16x4v-minmax-fp32-rvv.c
@@ -23,7 +23,7 @@ void xnn_qs8_qc8w_gemm_minmax_fp32_ukernel_16x4v__rvv(
     const int8_t* restrict a,
     size_t a_stride,
     const void* restrict w,
-    int8_t* restrict c,
+    int8_t* restrict c,     
     size_t cm_stride,
     size_t cn_stride,
     const union xnn_qs8_qc8w_conv_minmax_params params[restrict XNN_MIN_ELEMENTS(1)])
@@ -31,72 +31,68 @@ void xnn_qs8_qc8w_gemm_minmax_fp32_ukernel_16x4v__rvv(
   assert(mr != 0);
   assert(nc != 0);
   assert(kc != 0);
-  printf("16x4v\n");
-  // printf("w=%p\n", w);
-  // printf("c=%p\n", c);
+  printf("16x4v; mr=%zu, nc=%zu, kc=%zu\n", mr, nc, kc);
+  printf("a_stride=%zu, cm_stride=%zu, cn_stride=%zu\n", a_stride, cm_stride, cn_stride);
   int8_t* c0 = c;
   
   size_t nr;
-  __asm__ volatile("vsetvli %0, zero, e32, m4, ta, ma" : "=r"(nr));
+  __asm__ volatile("vsetvli %0, zero, e8, m1, ta, ma" : "=r"(nr));
   size_t vl = nr;
-
   const int32_t output_min_less_zero_point = (int32_t) params->fp32_scalar.output_min - (int32_t) params->fp32_scalar.output_zero_point;
   const int32_t output_max_less_zero_point = (int32_t) params->fp32_scalar.output_max - (int32_t) params->fp32_scalar.output_zero_point;
   const int32_t output_zero_point = params->fp32_scalar.output_zero_point;
   do {
-    if XNN_UNLIKELY(nc < nr) {
-      __asm__ volatile("vsetvli %0, %1, e32, m4, ta, ma" : "=r"(vl) : "r"(nc));
-      
-    }
-    nc = nc - vl;
-
+    printf("nc=%zu, nr=%zu, vl=%zu\n", nc, nr, vl);
+    __asm__ volatile("vsetvli zero, %0, e8, m1, ta, ma" : : "r"(vl));
     __asm__ volatile("vle32.v v0, (%0)" : : "r"((const int32_t*)w));
     OPMVINBCAST(m0, v0); // broadcast channel-wise bias
-    // printf("w + %d; ", ((const int32_t*) w + nr) - (const int32_t*) w);
     w = (const int32_t*) w + nr;
-
+    
     int8_t* am = a;
-    __asm__ volatile("vsetvli zero, %0, e8, m1, ta, ma" : : "r"(vl));
     size_t k = kc;
     do {
+      // __asm__ volatile("vlse8.v v4, (%0), %1" : : "r"(am), "r"(a_stride));
       __asm__ volatile("vlse8.v v4, (%0), %1" : : "r"(am), "r"(a_stride));
-      __asm__ volatile("vle8.v v5, (%0)" : : "r"(w));
-      VOPACC(m0, v5, v4);
-      am = (const int8_t*) am++;
-      // printf("w + %d; ", ((const int8_t*) w + nr) - (const int8_t*) w);
+      __asm__ volatile("vle8.v v6, (%0)" : : "r"(w));
+      VOPACC(m0, v6, v4);
+      am = (int8_t*) ((uintptr_t) am + 1);
       w = (const int8_t*) w + nr;
       k -= sizeof(int8_t);
     } while (k != 0);
     
+    if XNN_UNLIKELY(nc < nr) {
+      __asm__ volatile("vsetvli %0, %1, e8, m1, ta, ma" : "=r"(vl) : "r"(nc));
+    }
+    nc = nc - vl;
+    printf("nc=%zu, vl=%zu\n", nc, vl);
     //v4 <- vscale
-    __asm__ volatile("vsetvli zero, %0, e32, m4, ta, ma" : : "r"(vl));
-    __asm__ volatile("vle32.v v8, (%0)" : : "r"((const float*) w));
+    __asm__ volatile("vle32.v v24, (%0)" : : "r"((const float*) w));
     // printf("w + %d; ", ((const float*) w + nr) - (const float*) w);
     w = (const float*) w + nr;
-
+    
     int8_t* cm = c0;
     for (size_t r=0; r<mr; r++) {
       // printf("c(%zu)=%p\n", r, cm);
 
-      //v12 <- vopacc
+      //v0 <- vopacc
       __asm__ volatile("vsetvli zero, %0, e32, m4, ta, ma" : : "r"(vl));
-      VMV_VR(v12, r, m0); // move row r of m1 into v12
-      __asm__ volatile("vfcvt.f.x.v	v12,v12");
-      //v12 <- vopacc * vscale
-      __asm__ volatile("vfmul.vv	v12,v12,v8");
+      VMV_VR(v0, r, m0); // move row r of m1 into v0
+      __asm__ volatile("vfcvt.f.x.v	v0,v0");
+      //v0 <- vopacc * vscale
+      __asm__ volatile("vfmul.vv	v0,v0,v24");
       
-      //v12 <- minmax
-      __asm__ volatile("vfmax.vf	v12,v12,%0" : : "f"((float) output_min_less_zero_point));
-      __asm__ volatile("vfmin.vf	v12,v12,%0" : : "f"((float) output_max_less_zero_point));
+      //v0 <- minmax
+      __asm__ volatile("vfmax.vf	v0,v0,%0" : : "f"((float) output_min_less_zero_point));
+      __asm__ volatile("vfmin.vf	v0,v0,%0" : : "f"((float) output_max_less_zero_point));
       
       __asm__ volatile("vsetvli zero, %0, e16, m2, ta, ma" : : "r"(vl));
-      __asm__ volatile("vfncvt.x.f.w	v12,v12");
-      __asm__ volatile("vadd.vx	v12,v12,%0" : : "r"((int16_t) output_zero_point));
+      __asm__ volatile("vfncvt.x.f.w	v0,v0");
+      __asm__ volatile("vadd.vx	v0,v0,%0" : : "r"((int16_t) output_zero_point));
       
       __asm__ volatile("vsetvli zero, %0, e8, m1, ta, ma" : : "r"(vl));
-      __asm__ volatile("vncvt.x.x.w	v12,v12");
+      __asm__ volatile("vncvt.x.x.w	v0,v0");
       
-      __asm__ volatile("vse8.v	v12, (%0)" : : "r"(cm));
+      __asm__ volatile("vse8.v	v0, (%0)" : : "r"(cm));
       cm = (int8_t*) ((uintptr_t) cm + cm_stride);
     }
     // printf("c0=%p\n", c0);
